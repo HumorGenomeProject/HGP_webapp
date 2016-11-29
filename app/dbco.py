86902192293bd021118e5f9cba0ea2506e80ef29
manoajk <manoajk@yahoo.com>
2016-11-29 14:30:34 -0500

    Built recommender based on categories and rating

--- app/dbco.py
+++ app/dbco.py
@@ -1,8 +1,10 @@
 from pymongo import MongoClient, errors
 
 WEBAPP_DBNAME = 'hgp_webapp'
+JOKERZ_DBNAME = 'hgp_jokerz'
 
 # TODO: Move this to a config file.
 localConnection = 'mongodb://127.0.0.1:27017/'
 client = MongoClient(localConnection)
 db = client[WEBAPP_DBNAME]
+db_jokerz = client[JOKERZ_DBNAME]
